581ea4caa3f006cbf9f9ba3e634b5b5b00b0f782
Seneca Sisneros <seneca.sisneros@yahoo.com>
2016-10-29 10:05:36 -0700

    fixed and update data flow to MoonDetail component

--- src/components/MoonDetails.js
+++ src/components/MoonDetails.js
@@ -1,23 +1,28 @@
 import React from "react";
 import moment from "moment";
 
-const MoonDetails = ( {rise, newData, name, transitISO, riseISO, setISO, angle, illum, phase, dateTimeISO, city, state } ) => {
-  var transit = moment(transitISO).format('llll');
-  var rise = moment(riseISO).format('llll');
-  var set = moment(setISO).format('llll');
-  var dateTime = moment(dateTimeISO).format('llll');
+const MoonDetails = ( {moon, phase, place, date} ) => {
+  const { rise, transitISO, riseISO, setISO} = moon;
+  const { name, angle, illum } = phase;
+  const { dateTimeISO } = date;
+  let city = place.name
+  const { state } = place
+
+  let transit = moment(transitISO).format('llll');
+  let rise1 = moment(riseISO).format('llll');
+  let set = moment(setISO).format('llll');
+  let dateTime = moment(dateTimeISO).format('llll');
   return (
     <div>
-    <p className="details">Current Date and Time: { dateTime }</p>
-    <p className="details">{ city }, {state}</p>
-    <p className="details">Moon Name: { name }</p>
-    {/* <p className="details">Rise: { rise }</p> */}
-      <p className="details">Transit: { transit }</p>
       <p className="details">Moon Rise: { rise }</p>
+      <p className="details">Rise: { rise1 }</p>
+      <p className="details">Transit: { transit }</p>
       <p className="details">Moon Set: { set }</p>
+      <p className="details">Moon Name: { name }</p>
       <p className="details">Moon Angle: { angle }</p>
       <p className="details">Moon illumination: { illum }</p>
-      <p className="details">Moon Phase: { phase }</p>
+      <p className="details">Current Date and Time: { dateTime }</p>
+    <p className="details">{city} {state}</p>
       <img className="icon" src="http://www.freeiconspng.com/uploads/moon-icon-5.jpg"/>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -12,53 +12,38 @@ class App extends Component {
     super(props);
 
     this.state = {
-      newData: null
+      moon: [],
+      phase: [],
+      place: [],
+      date: []
   };
   this.citySearch('oakland');
 
 }
   citySearch(city) {
-    axios.get(`https://api.aerisapi.com/sunmoon/${city}?from=2016-01-01&to=2016-010-31&limit=31&client_id=RJ483JTv5hKMuW09hMzYF&client_secret=3tyzbporWFb6kv20yUQ2Jq1jNJfLeIAOYE4ZdUqA`)
+    axios.get(`https://api.aerisapi.com/sunmoon/92532?&client_id=RJ483JTv5hKMuW09hMzYF&client_secret=3tyzbporWFb6kv20yUQ2Jq1jNJfLeIAOYE4ZdUqA`)
       .then(response => {
-        let { rise, transitISO, riseISO, setISO } = response.data.response[0].moon
-        let { name, angle, illum, phase } = response.data.response[0].moon.phase
-        let {  dateTimeISO } = response.data.response[0]
-        let {  state } = response.data.response[0].place
-        let city = response.data.response[0].place.name
-          // let newData = response.data.response[0].moon.rise
             this.setState({
-              newData: response,
-              rise: rise,
-              name: name,
-              transitISO: transitISO,
-              riseISO: riseISO,
-              setISO: setISO,
-              angle: angle,
-              illum: illum,
-              dateTimeISO: dateTimeISO,
-              phase: phase,
-              city: city,
-              state: state
+              moon: response.data.response[0].moon,
+              phase: response.data.response[0].moon.phase,
+              place: response.data.response[0].place,
+              date: response.data.response[0]
             })
       })
       .catch(function (error) {
         console.log(error);
       });
   }
-// }
   render(){
-
     return (
       <div>
         <h1 className="title">Lunar Love</h1>
           <SearchBar onSearchTermChange={city => this.citySearch(city)}/>
-          <MoonDetails rise={this.state.rise} newData={this.state.newData} name={this.state.name} transitISO={this.state.transitISO} riseISO={this.state.riseISO} setISO={this.state.setISO} angle={this.state.angle} illum={this.state.illum} phase={this.state.phase}
-          dateTimeISO={this.state.dateTimeISO} city={this.state.city} state={this.state.state}/>
+          <MoonDetails  moon={this.state.moon} phase={this.state.phase} place={this.state.place} date={this.state.date}/>
           <MoonInfo />
       </div>
     );
   }
 }
-
 
 ReactDOM.render(<App />, document.querySelector('.container'))
