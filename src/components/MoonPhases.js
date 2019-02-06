c2e0a37d2ff716cff12965a555b5c84756fb2eb4
Seneca Sisneros <seneca.sisneros@yahoo.com>
2019-02-06 07:11:08 -0800

    Update Aeris api

--- src/components/MoonPhases.js
+++ src/components/MoonPhases.js
@@ -2,38 +2,67 @@ import React from "react";
 import moment from "moment";
 import lodash from "lodash";
 
-const MoonPhases = ( {PhaseOne, PhaseTwo, PhaseThree, PhaseFour} ) => {
+const MoonPhases = ({ PhaseOne, PhaseTwo, PhaseThree, PhaseFour }) => {
   let oneName = PhaseOne.name;
-  let oneTime = moment.unix(PhaseOne.timestamp).add('hours').format('LLL');
+  console.log("oneName:", oneName);
+  let oneTime = moment
+    .unix(PhaseOne.timestamp)
+    .add("hours")
+    .format("LLL");
   let twoName = PhaseTwo.name;
-  let twoTime = moment.unix(PhaseTwo.timestamp).add('hours').format('LLL');
+  let twoTime = moment
+    .unix(PhaseTwo.timestamp)
+    .add("hours")
+    .format("LLL");
   let threeName = PhaseThree.name;
-  let threeTime = moment.unix(PhaseThree.timestamp).add('hours').format('LLL');
+  let threeTime = moment
+    .unix(PhaseThree.timestamp)
+    .add("hours")
+    .format("LLL");
   let fourName = PhaseFour.name;
-  let fourTime = moment.unix(PhaseFour.timestamp).add('hours').format('LLL');
+  let fourTime = moment
+    .unix(PhaseFour.timestamp)
+    .add("hours")
+    .format("LLL");
   return (
     <div className="container phases">
       <h4 className="titleMoonPhases">Next moon phases:</h4>
       <div className="row">
         <div className="col-lg-3 fourPhases">
-            {_.startCase(_.camelCase(oneName))}: {oneTime}
-            <img width="175" height="175" src={`https://s3.amazonaws.com/lunarlove/${oneName}.jpg`}/>
+          {_.startCase(_.camelCase(oneName))}: {oneTime}
+          <img
+            width="175"
+            height="175"
+            src={`https://s3.amazonaws.com/lunarlove/${oneName}.jpg`}
+          />
         </div>
         <div className="col-lg-3 fourPhases">
-            {_.startCase(_.camelCase(twoName))}: {twoTime}
-            <img width="175" height="175" src={`https://s3.amazonaws.com/lunarlove/${twoName}.jpg`}/>
+          {_.startCase(_.camelCase(twoName))}: {twoTime}
+          <img
+            width="175"
+            height="175"
+            src={`https://s3.amazonaws.com/lunarlove/${twoName}.jpg`}
+          />
         </div>
         <div className="col-lg-3 fourPhases">
-            {_.startCase(_.camelCase(threeName))}: {threeTime}
-            <img width="175" height="175" src={`https://s3.amazonaws.com/lunarlove/${threeName}.jpg`}/>
+          {_.startCase(_.camelCase(threeName))}: {threeTime}
+          <img
+            width="175"
+            height="175"
+            src={`https://s3.amazonaws.com/lunarlove/${threeName}.jpg`}
+          />
         </div>
         <div className="col-lg-3 fourPhases">
-            {_.startCase(_.camelCase(fourName))}: {fourTime}
-            <img width="175" height="175" src={`https://s3.amazonaws.com/lunarlove/${fourName}.jpg`}/>
+          {_.startCase(_.camelCase(fourName))}: {fourTime}
+          <img
+            width="175"
+            height="175"
+            src={`https://s3.amazonaws.com/lunarlove/${fourName}.jpg`}
+          />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default MoonPhases;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import _ from "lodash";
-import moment from 'moment';
+import moment from "moment";
 
 import MoonDetails from "./components/MoonDetails";
 import MoonInfo from "./components/MoonInfo";
@@ -23,48 +23,82 @@ class App extends Component {
       PhaseThree: [],
       PhaseFour: []
     };
-    this.zipcodeSearch('92532');
-    this.moonPhaseSearch('92532');
+    this.zipcodeSearch("92532");
+    this.moonPhaseSearch("92532");
   }
 
   zipcodeSearch(zipcode) {
-    axios.get(`https://api.aerisapi.com/sunmoon/${zipcode}?&client_id=34yyWmaididZo1zz7IOfl&client_secret=yLfxyDp6r6Y5Fn6u8XYjpcbyljVBBI0INHyTFykJ`).then(response => {
-      this.setState({moon: response.data.response[0].moon, phase: response.data.response[0].moon.phase, place: response.data.response[0].place, date: response.data.response[0]})
-    }).catch(function(error) {
-      console.log(error);
-    });
+    axios
+      .get(
+        `https://api.aerisapi.com/sunmoon/${zipcode}?&client_id=QaRtBZ7n0JfmLAQVluuZU&client_secret=BNKhYUkGgYS8oKx53vUH9xmba3LupYXyXvRpIoGm`
+      )
+      .then(response => {
+        this.setState({
+          moon: response.data.response[0].moon,
+          phase: response.data.response[0].moon.phase,
+          place: response.data.response[0].place,
+          date: response.data.response[0]
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   moonPhaseSearch(zipcode) {
-    axios.get(`https://api.aerisapi.com/sunmoon/moonphases?&limit=4/90001?&client_id=34yyWmaididZo1zz7IOfl&client_secret=yLfxyDp6r6Y5Fn6u8XYjpcbyljVBBI0INHyTFykJ`).then(response => {
-      this.setState({PhaseOne: response.data.response[0], PhaseTwo: response.data.response[1], PhaseThree: response.data.response[2], PhaseFour: response.data.response[3]})
-    }).catch(function(error) {
-      console.log(error);
-    });
+    axios
+      .get(
+        `https://api.aerisapi.com/sunmoon/moonphases?&limit=4/90001?&client_id=QaRtBZ7n0JfmLAQVluuZU&client_secret=BNKhYUkGgYS8oKx53vUH9xmba3LupYXyXvRpIoGm`
+      )
+      .then(response => {
+        this.setState({
+          PhaseOne: response.data.response[0],
+          PhaseTwo: response.data.response[1],
+          PhaseThree: response.data.response[2],
+          PhaseFour: response.data.response[3]
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   render() {
-    const zipcodeSearch = _.debounce((zipcode) => {
-      this.zipcodeSearch(zipcode)
+    const zipcodeSearch = _.debounce(zipcode => {
+      this.zipcodeSearch(zipcode);
     }, 300);
 
     return (
       <div>
         <div className="res">
-          <img id="moonLogo" src="./style/moonLogo.png" alt=""/>
+          <img id="moonLogo" src="./style/moonLogo.png" alt="" />
           <span className="title">The Mysterious Moon</span>
         </div>
-        <SearchBar onSearchTermChange={zipcodeSearch} place={this.state.place} date={this.state.date}/>
-        <MoonDetails moon={this.state.moon} phase={this.state.phase} place={this.state.place} date={this.state.date}/>
+        <SearchBar
+          onSearchTermChange={zipcodeSearch}
+          place={this.state.place}
+          date={this.state.date}
+        />
+        <MoonDetails
+          moon={this.state.moon}
+          phase={this.state.phase}
+          place={this.state.place}
+          date={this.state.date}
+        />
         <div>
-          <MoonInfo phase={this.state.phase}/>
+          <MoonInfo phase={this.state.phase} />
         </div>
         <div>
-          <MoonPhases PhaseOne={this.state.PhaseOne} PhaseTwo={this.state.PhaseTwo} PhaseThree={this.state.PhaseThree} PhaseFour={this.state.PhaseFour}/>
+          <MoonPhases
+            PhaseOne={this.state.PhaseOne}
+            PhaseTwo={this.state.PhaseTwo}
+            PhaseThree={this.state.PhaseThree}
+            PhaseFour={this.state.PhaseFour}
+          />
         </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector(".container"));
